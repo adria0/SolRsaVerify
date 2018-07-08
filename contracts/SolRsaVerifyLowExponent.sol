94345155a808d44f736fd8b389d2b0586221435b
Bjorn van der Laan <bjorn.vdlaan@gmail.com>
2018-07-08 19:17:09 +0200

    Added verification library for low exponents

---
+++ contracts/SolRsaVerifyLowExponent.sol
@@ -0,0 +1,146 @@
+pragma solidity ^0.4.19;
+
+/*
+    Copyright 2016, Adrià Massanet
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Checked results with FIPS test vectors
+    https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Algorithm-Validation-Program/documents/dss/186-2rsatestvectors.zip
+    file SigVer15_186-3.rsp
+
+ */
+
+
+library SolRsaVerifySmallExponent {
+
+    /** @dev Verifies a PKCSv1.5 SHA256 signature
+      * @param _sha256 is the sha256 of the data
+      * @param _s is the signature
+      * @param _e is the exponent
+      * @param _m is the modulus
+      * @return 0 if success, >0 otherwise
+    */
+    function pkcs1Sha256Verify(bytes32 _sha256, bytes _s, uint _e, bytes _m) public view returns (bool) {
+
+        uint8[19] memory sha256Prefix = [
+        0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x05, 0x00, 0x04, 0x20
+        ];
+
+        require(_m.length >= sha256Prefix.length+_sha256.length+11);
+
+        uint i;
+
+        /// decipher
+        uint decipherlen = _m.length;
+        bytes memory decipher = new bytes(decipherlen);
+
+        decipher = modexp(_s, _e, _m);
+
+
+        /// 0x00 || 0x01 || PS || 0x00 || DigestInfo
+        /// PS is padding filled with 0xff
+        //  DigestInfo ::= SEQUENCE {
+        //     digestAlgorithm AlgorithmIdentifier,
+        //     digest OCTET STRING
+        //  }
+
+        uint paddingLen = decipherlen - 3 - sha256Prefix.length - 32;
+
+        if (decipher[0] != 0 || decipher[1] != 1) {
+            return false;
+        }
+        for (i = 2;i<2+paddingLen;i++) {
+            if (decipher[i] != 0xff) {
+                return false;
+            }
+        }
+        if (decipher[2+paddingLen] != 0) {
+            return false;
+        }
+        for (i = 0;i<sha256Prefix.length;i++) {
+            if (uint8(decipher[3+paddingLen+i])!=sha256Prefix[i]) {
+                return false;
+            }
+        }
+        for (i = 0;i<_sha256.length;i++) {
+            if (decipher[3+paddingLen+sha256Prefix.length+i]!=_sha256[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /** @dev Verifies a PKCSv1.5 SHA256 signature
+      * _data
+      * @param _s is the signature
+      * @param _e is the exponent
+      * @param _m is the modulus
+      * @return 0 if success, >0 otherwise
+    */
+    function pkcs1Sha256VerifyRaw(bytes _data, bytes _s, uint _e, bytes _m) public view returns (bool) {
+        return pkcs1Sha256Verify(sha256(_data),_s,_e,_m);
+    }
+
+    function modexp(bytes base, uint exponent, bytes modulus) internal returns (bytes output) {
+        //bytes memory modulus = bytes(modulus_str);
+        uint base_length = base.length;
+        uint modulus_length = modulus.length;
+
+        uint size = (32 * 3) + base_length + 32 + modulus_length;
+        bytes memory input = new bytes(size);
+        output = new bytes(modulus_length);
+
+        assembly {
+            mstore(add(input, 32), base_length)
+            mstore(add(input, 64), 32)
+            mstore(add(input, 96), modulus_length)
+
+            mstore(add(input, add(128, base_length)), exponent)
+        }
+
+        memcopy(base, 0, input, 96, base_length);
+        memcopy(modulus, 0, input, 96 + base_length + 32, modulus_length);
+
+        assembly {
+            pop(call(gas(), 5, 0, add(input, 32), size, add(output, 32), modulus_length))
+        }
+    }
+
+    function memcopy(bytes src, uint srcoffset, bytes dst, uint dstoffset, uint len) pure internal {
+        assembly {
+            src := add(src, add(32, srcoffset))
+            dst := add(dst, add(32, dstoffset))
+
+        // copy 32 bytes at once
+            for
+            {}
+            iszero(lt(len, 32))
+            {
+                dst := add(dst, 32)
+                src := add(src, 32)
+                len := sub(len, 32)
+            }
+            {mstore(dst, mload(src))}
+
+        // copy the remainder (0 < len < 32)
+            let mask := sub(exp(256, sub(32, len)), 1)
+            let srcpart := and(mload(src), not(mask))
+            let dstpart := and(mload(dst), mask)
+            mstore(dst, or(srcpart, dstpart))
+        }
+    }
+
+}
