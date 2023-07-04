8792a68bff1e52eadbc948dc1add63b8cb8ebf46
adria0 <adria0@nowhere>
2023-07-04 09:25:52 +0200

    moved to forge, solidity 0.8

--- src/RsaVerify.sol
+++ src/RsaVerify.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
 /*
     Copyright 2016, Adrià Massanet
@@ -22,58 +22,8 @@ pragma solidity ^0.6.0;
     
  */
 
-library SolRsaVerify {
+library RsaVerify {
 
-    function memcpy(uint _dest, uint _src, uint _len) pure internal {
-        // Copy word-length chunks while possible
-        for ( ;_len >= 32; _len -= 32) {
-            assembly {
-                mstore(_dest, mload(_src))
-            }
-            _dest += 32;
-            _src += 32;
-        }
-
-        // Copy remaining bytes
-        uint mask = 256 ** (32 - _len) - 1;
-        assembly {
-            let srcpart := and(mload(_src), not(mask))
-            let destpart := and(mload(_dest), mask)
-            mstore(_dest, or(destpart, srcpart))
-        }
-    }
-
-    
-    function join(
-	bytes memory _s, bytes memory _e, bytes memory _m
-    ) pure internal returns (bytes memory) {
-        uint inputLen = 0x60+_s.length+_e.length+_m.length;
-        
-        uint slen = _s.length;
-        uint elen = _e.length;
-        uint mlen = _m.length;
-        uint sptr;
-        uint eptr;
-        uint mptr;
-        uint inputPtr;
-        
-        bytes memory input = new bytes(inputLen);
-        assembly {
-            sptr := add(_s,0x20)
-            eptr := add(_e,0x20)
-            mptr := add(_m,0x20)
-            mstore(add(input,0x20),slen)
-            mstore(add(input,0x40),elen)
-            mstore(add(input,0x60),mlen)
-            inputPtr := add(input,0x20)
-        }
-        memcpy(inputPtr+0x60,sptr,_s.length);        
-        memcpy(inputPtr+0x60+_s.length,eptr,_e.length);        
-        memcpy(inputPtr+0x60+_s.length+_e.length,mptr,_m.length);
-
-        return input;
-    }
-    
     /** @dev Verifies a PKCSv1.5 SHA256 signature
       * @param _sha256 is the sha256 of the data
       * @param _s is the signature
@@ -81,10 +31,10 @@ library SolRsaVerify {
       * @param _m is the modulus
       * @return 0 if success, >0 otherwise
     */    
-    function pkcs1Sha256Verify(
+    function pkcs1Sha256(
         bytes32 _sha256,
         bytes memory _s, bytes memory _e, bytes memory _m
-    ) public view returns (uint) {
+    ) public view returns (bool) {
         
         uint8[19] memory sha256Prefix = [
             0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x05, 0x00, 0x04, 0x20
@@ -95,14 +45,19 @@ library SolRsaVerify {
         uint i;
 
         /// decipher
-        bytes memory input = join(_s,_e,_m);
+        bytes memory input = bytes.concat(
+            bytes32(_s.length),
+            bytes32(_e.length),
+            bytes32(_m.length),
+            _s,_e,_m
+        );
         uint inputlen = input.length;
 
         uint decipherlen = _m.length;
         bytes memory decipher = new bytes(decipherlen);
         assembly {
             pop(staticcall(sub(gas(), 2000), 5, add(input,0x20), inputlen, add(decipher,0x20), decipherlen))
-	}
+	    }
         
         /// 0x00 || 0x01 || PS || 0x00 || DigestInfo
         /// PS is padding filled with 0xff
@@ -110,32 +65,32 @@ library SolRsaVerify {
         //     digestAlgorithm AlgorithmIdentifier,
         //     digest OCTET STRING
         //  }
-        
+
         uint paddingLen = decipherlen - 3 - sha256Prefix.length - 32;
         
         if (decipher[0] != 0 || uint8(decipher[1]) != 1) {
-            return 1;
+            return false;
         }
         for (i = 2;i<2+paddingLen;i++) {
             if (decipher[i] != 0xff) {
-                return 2;
+                return false;
             }
         }
         if (decipher[2+paddingLen] != 0) {
-            return 3;
+            return false;
         }
         for (i = 0;i<sha256Prefix.length;i++) {
             if (uint8(decipher[3+paddingLen+i])!=sha256Prefix[i]) {
-                return 4;
+                return false;
             }
         }
         for (i = 0;i<_sha256.length;i++) {
             if (decipher[3+paddingLen+sha256Prefix.length+i]!=_sha256[i]) {
-                return 5;
+                return false;
             }
         }
 
-        return 0;
+        return true;
     }
 
     /** @dev Verifies a PKCSv1.5 SHA256 signature
@@ -145,11 +100,11 @@ library SolRsaVerify {
       * @param _m is the modulus
       * @return 0 if success, >0 otherwise
     */    
-    function pkcs1Sha256VerifyRaw(
+    function pkcs1Sha256Raw(
         bytes memory _data, 
         bytes memory _s, bytes memory _e, bytes memory _m
-    ) public view returns (uint) {
-        return pkcs1Sha256Verify(sha256(_data),_s,_e,_m);
+    ) public view returns (bool) {
+        return pkcs1Sha256(sha256(_data),_s,_e,_m);
     }
 
 }
--- truffle-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  compilers: {
-    solc: {
-      version: "0.6.0",
-    },
-  },
-};
